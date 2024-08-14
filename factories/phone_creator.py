8adf6b1e2459682428827256ce1cd5a23878b7ae
Fatima Ferreira <ifatinha99@gmail.com>
2024-08-14 13:25:51 -0300

    refatoração da classe

--- factories/phone_creator.py
+++ factories/phone_creator.py
@@ -1,3 +1,30 @@
+"""
+Este módulo fornece a classe `PhoneCreator`, que é responsável por criar objetos
+de telefone com base nas entradas do usuário.
+
+A classe `PhoneCreator` oferece métodos para:
+- Exibir um menu de seleção de tipo de telefone.
+- Solicitar e validar a entrada do usuário para criar um novo telefone.
+- Fornecer um menu para adicionar telefones e processar a entrada até
+que o usuário escolha sair.
+
+A classe utiliza as seguintes componentes:
+- `Phone`: Classe que representa um telefone e seus atributos.
+- `PhonesTypes`: Enumeração que define os tipos de telefone disponíveis.
+
+Funções principais:
+- `PhoneCreator.get_phone_type()`: Exibe um menu para o usuário escolher o tipo
+   de telefone e retorna o tipo selecionado.
+- `PhoneCreator.get_phone()`: Solicita ao usuário os dados do telefone e
+   cria uma instância de `Phone`.
+- `PhoneCreator.creator_new_phone()`: Permite ao usuário adicionar novos telefones e
+   continua a solicitar entradas até que o usuário opte por sair.
+
+Exemplo de uso:
+    phone = PhoneCreator.get_phone()
+    print(phone)
+"""
+
 from classes.contact import Contact
 from classes.phone import Phone
 from classes.email import Email
@@ -6,19 +33,42 @@ from enums.phones_types import PhonesTypes
 
 class PhoneCreator():
 
+    """
+    Classe responsável por criar instâncias de Phone com base nas entradas do usuário.
+    Oferece um menu para selecionar o tipo de telefone e inserir os dados necessários
+    para criar o objeto Phone.
+    """
+
     @staticmethod
-    def _generate_prompt():
-        return """################ MENU ################
+    def _prompt_type_phone():
+        """
+        Retorna o menu de seleção de tipo de telefone como uma string.
+
+        Returns:
+            str: O menu de seleção de tipo de telefone.
+        """
+
+        return """  # MENU ################
         [1] - Celular
         [2] - Fixo
         [3] - VoIP
         [4] - Fax
         [5] - Satélite
         [6] - Emergência
-        ==> """
+        == > """
 
     @staticmethod
     def _get_user_input(prompt):
+        """
+        Solicita uma entrada numérica do usuário com base em um prompt.
+
+        Args:
+            prompt (str): O texto a ser exibido ao usuário.
+
+        Returns:
+            int or None: O número inteiro inserido pelo usuário ou None em caso de erro.
+        """
+
         try:
             return int(input(prompt))
         except ValueError:
@@ -27,6 +77,13 @@ class PhoneCreator():
 
     @staticmethod
     def get_phone_type():
+        """
+        Exibe um menu para o usuário selecionar o tipo de telefone e retorna o tipo correspondente.
+
+        Returns:
+            PhonesTypes: O tipo de telefone selecionado pelo usuário.
+        """
+
         dict_types_phones = {
             1: PhonesTypes.MOBILE,
             2: PhonesTypes.LANDLINE,
@@ -36,7 +93,7 @@ class PhoneCreator():
             6: PhonesTypes.EMERGENCIA,
         }
 
-        prompt = PhoneCreator._generate_prompt()
+        prompt = PhoneCreator._prompt_type_phone()
 
         while True:
 
@@ -48,6 +105,13 @@ class PhoneCreator():
 
     @staticmethod
     def get_phone():
+        """
+        Solicita ao usuário os dados de um telefone (código do país, DDD, número e tipo) 
+        e cria uma instância de Phone.
+
+        Returns:
+            Phone: A instância de Phone criada com os dados fornecidos pelo usuário.
+        """
         country_code = input("Código do pais: ")
         ddd = input("DDD: ")
         phone_number = input("Número: ")
@@ -57,28 +121,34 @@ class PhoneCreator():
                      type_number=type_number)
 
     @staticmethod
-    def new_phone_prompt():
-        return """################### NOVO TELEFONE ###################
-        [1] - Novo telefone
-        [2] - Sair
-        ==> """
+    def _prompt_add_phone():
+        """
+        Retorna o menu de opção para adicionar um novo telefone.
+
+        Returns:
+            str: O menu para adicionar ou sair.
+        """
+
+        return """  # NOVO TELEFONE ###################
+        [1] - Adicionar telefone
+        [0] - Sair
+        == > """
 
     @staticmethod
     def creator_new_phone():
+        """
+        Permite ao usuário adicionar novos telefones. Continua a pedir a inserção de 
+        telefones até que o usuário escolha sair.
+        """
+
+        prompt = PhoneCreator._prompt_add_phone()
 
         while True:
 
-            prompt = PhoneCreator.new_phone_prompt()
-
-            try:
-                option = PhoneCreator._get_user_input(prompt)
-            except ValueError:
-                print("@@@ Entrada inválida, por favor insira um número. @@@")
-                continue
+            option = PhoneCreator._get_user_input(prompt)
 
             if option == 1:
                 phone = PhoneCreator.get_phone()
-                return phone
             elif option == 0:
                 break
             else:
